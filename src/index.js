7795e587641750f864457291635857c26005ed17
Tusar <tusarahmed141@gmail.com>
2022-10-14 11:41:55 +0600

    First react app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import App from './App';
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -16,3 +17,7 @@
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
+ReactDOM.render(
+  <App /> ,
+  document.getElementById("root")
+)
