18c51d46bdd68f7c8f80a4a6dab0100f96bb91a3
Tusar <tusarahmed141@gmail.com>
2022-10-14 16:05:54 +0600

    Added link for website using react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Products from './Pages/Products/Products'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./Components/Navigation/Navigation";
+import Cart from "./Pages/Cart/Cart";
+import Home from "./Pages/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
+import Products from "./Pages/Products/Products";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='/products' element={<Products />} />
-    </Routes>
-  )
-}
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <>
+      <header className="bg-white">
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex-1 md:flex md:items-center md:gap-12">
+              <Link className="block text-teal-600" to="/">
+                <span className="sr-only">Home</span>
+                <img src="images/logo.png" alt="website logo" />
+              </Link>
+            </div>
 
-export default Navigation
+            <div className="md:flex md:items-center md:gap-12">
+              <nav
+                className="hidden md:block"
+                ariaLabelledby="header-navigation"
+              >
+                <h2 className="sr-only" id="header-navigation">
+                  Header navigation
+                </h2>
+
+                <ul className="flex items-center gap-6 text-sm">
+                  <li>
+                    <Link
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      to="/home"
+                    >
+                      Home
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      to="/products"
+                    >
+                      Products
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      to="/login"
+                    >
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      to="/cart"
+                    >
+                      Cart
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+
+              <div className="flex items-center gap-4">
+                <div className="block md:hidden">
+                  <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navigation;
