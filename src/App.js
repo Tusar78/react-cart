16612c8f89ac167540febcc6cd6cd70b6c048dfb
Tusar <tusarahmed141@gmail.com>
2022-10-14 15:29:07 +0600

    React router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Products from './Pages/Products/Products'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/products' element={<Products />} />
+    </Routes>
   )
 }
 
